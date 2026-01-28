053b44773e902ec85402dd6d4932b35267630d74
mohammad <ghavidelheidarei81@gmail.com>
2026-01-28 04:27:48 +0330

    feat: Complete project restructuring with new architecture

--- js/global.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const settings = JSON.parse(localStorage.getItem("settings"));
-
-    if (settings && settings.darkMode) {
-        document.body.classList.add("dark");
-    }
-});
